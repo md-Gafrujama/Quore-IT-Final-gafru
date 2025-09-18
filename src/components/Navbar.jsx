09386edc727bd8c8658a9f09978de7da829fffae
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-09-18 22:18:35 +0530

    added quore it blogs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
     { name: 'FIND TECH JOBS', href: '/Find-tech-jobs', hasDropdown: true },
     { name: 'FIND TECH TALENT', href: '/Find-tech-talent' },
     { name: 'WHAT WE DO', href: '/What-we-do' },
-    { name: 'NEWS & EVENTS', href: '/News-and-events' },
+    { name: 'NEWS & EVENTS', href: 'https://quore-it-ai-blogs.vercel.app/' },
     { name: 'ABOUT US', href: '/About-Us' },
     { name: 'CONTACT US', href: '/Contact-us' },
   ];
